c4e1719864176eb32dafaad369ecbf4c2cc68593
Joe Dettmar <joedettmar@gmail.com>
2017-11-03 19:08:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,4 @@ var playlist = {artistName: "songTitle"}
 function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName] = songTitle;
 return playlist;}
 
-function removeFromPlaylist(playlist, artistName) {delete playlist.artistName; return playlist;}
+function removeFromPlaylist(playlist, artistName) {delete playlist[artistName]; return playlist;}
